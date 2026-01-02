2e02c21396bf30d3328f3cd035dd40c419c809de
Brandon D. <84109908+BrandonDelM@users.noreply.github.com>
2026-01-02 15:36:35 -0800

    Made the code for the discord bot to be more repeatable and take less code

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ def check_for_changes(r, file):
 
     return is_notable(mismatches, f"{datetime.datetime.now().year}")
 
+
 class Client(discord.Client):
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
@@ -32,48 +33,41 @@ class Client(discord.Client):
         
         if message.content.startswith('hello'):
             await message.channel.send(f'Hi there {message.author}')
+    
+    async def automate_check(self, url, channel, file):
+        channel = self.get_channel(channel)
+        if  channel is None:
+            print(f"Error: No channel {channel} for {url}")
+            return
+
+        while not self.is_closed():
+            await asyncio.sleep(10)
+            r = request(url)
+            if r is None:
+                await channel.send("Failure to find calendar for psychological Sciences")
+                continue
+            
+            notables  = check_for_changes(r, file)
+
+            if len(notables) == 0:
+                await channel.send(f"No changes for {url}")
+            else:
+                compiled_notables = format_text(notables)
+                await channel.send(compiled_notables)
 
     async def  psychological_sciences_check(self):
         await self.wait_until_ready()
-        channel = self.get_channel(1456771381629812787)
-
+        url = "https://psychology.ucmerced.edu/events"
+        channel = 1456771381629812787
         file = "logs/ps_log.txt"
-        while not self.is_closed():
-            await asyncio.sleep(10)
-            if channel:
-                r = request("https://psychology.ucmerced.edu/events")
-                if r is None:
-                    await channel.send("Failure to find calendar for psychological Sciences")
-                    continue
-
-                notables = check_for_changes(r, file)
-                compiled_notables = format_text(notables)
-
-                if len(notables) == 0:
-                    await channel.send("No changes")
-                else:
-                    await channel.send(compiled_notables)
-
+        await self.automate_check(url, channel, file)
 
     async def cec_message(self):
         await self.wait_until_ready()
-        channel = self.get_channel(1456771381629812787)
+        channel = 1456771381629812787
         file = "logs/cec_log.txt"
-        while not self.is_closed():
-            await asyncio.sleep(10)
-            if channel:
-                r = request("https://cec.ucmerced.edu/calendar")
-                if r is None:
-                    await channel.send("Failure to find calendar for CEC")
-                    continue
-
-                notables = check_for_changes(r, file)
-                compiled_notables = format_text(notables)
-
-                if len(notables) == 0:
-                    await channel.send("No changes")
-                else:
-                    await channel.send(compiled_notables)
+        url  = "https://cec.ucmerced.edu/calendar"
+        await self.automate_check(url, channel, file)
 
 intents = discord.Intents.default()
 intents.message_content = True
