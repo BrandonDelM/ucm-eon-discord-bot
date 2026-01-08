d7dd148941c5782443517d2a4aef81715809b4dc
Brandon D. <84109908+BrandonDelM@users.noreply.github.com>
2026-01-07 18:06:54 -0800

    Made the code less repetitive by making a function for creating tasks

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ def check_for_changes(r, file, url, type):
         return rss_changes(r, file)
     elif type == "ics":
         return ics_change(r, file)
+    elif type == "youtube":
+        return rss_changes(r, file)
 
 class Client(discord.Client):
     async def on_ready(self):
@@ -50,43 +52,60 @@ class Client(discord.Client):
             await channel.send(new_events_text)
             await asyncio.sleep(3600)
 
+    async def get_tasks(self, file, type, offset):
+        tasks = []
+        df = is_csv_accessible(file)
+        if df is not None:
+            urls, channels, mentions, files = get_csv_columns(df)
+            for i in range(len(urls)):
+                task = self.loop.create_task(self.automate_check(urls[i], channels[i], files[i], type, delay_offset=offset*20))
+                offset += 1
+                tasks.append(task)
+        return tasks
 
     async def uc_merced_calendars_check(self):
         await self.wait_until_ready()
         tasks = []
-        n = 0
+        offset = 0
 
-        file = "./csvs/calendars.csv"
-        type = "calendar"
-        df = is_csv_accessible(file)
-        if df is not None:
-            df = is_csv_accessible(file)
-            urls, channels, mentions, files = get_csv_columns(df)
-            for i in range(len(urls)):
-                task = self.loop.create_task(self.automate_check(urls[i], channels[i], files[i], type, delay_offset=i*20))
-                n += 1
-                tasks.append(task)
+        tasks.extend(await self.get_tasks("./csvs/calendars.csv", "calendar", offset))
+        offset = len(tasks)
 
-        file = "./csvs/ics.csv"
-        type = "ics"
-        df = is_csv_accessible(file)
-        if df is not None:
-            urls, channels, mentions, files = get_csv_columns(df)
-            for i in range(len(urls)):
-                task = self.loop.create_task(self.automate_check(urls[i], channels[i], files[i], type, delay_offset=n*20))
-                n += 1
-                tasks.append(task)
+        tasks.extend(await self.get_tasks("./csvs/ics.csv", "ics", offset))
+        offset = len(tasks)
 
-        file = "./csvs/rss.csv"
-        type = "rss"
-        df = is_csv_accessible(file)
-        if df is not None:
-            df = is_csv_accessible(file)
-            urls, channels, mentions, files = get_csv_columns(df)
-            for i in range(len(urls)):
-                task = self.loop.create_task(self.automate_check(urls[i], channels[i], files[i], type, delay_offset=n*20))
-                n += 1
-                tasks.append(task)
+        tasks.extend(await self.get_tasks("./csvs/rss.csv", "rss", offset))
+        offset = len(tasks)
+        # file = "./csvs/calendars.csv"
+        # type = "calendar"
+        # df = is_csv_accessible(file)
+        # if df is not None:
+        #     urls, channels, mentions, files = get_csv_columns(df)
+        #     for i in range(len(urls)):
+        #         task = self.loop.create_task(self.automate_check(urls[i], channels[i], files[i], type, delay_offset=i*20))
+        #         n += 1
+        #         tasks.append(task)
+
+        # file = "./csvs/ics.csv"
+        # type = "ics"
+        # df = is_csv_accessible(file)
+        # if df is not None:
+        #     urls, channels, mentions, files = get_csv_columns(df)
+        #     for i in range(len(urls)):
+        #         task = self.loop.create_task(self.automate_check(urls[i], channels[i], files[i], type, delay_offset=n*20))
+        #         n += 1
+        #         tasks.append(task)
+
+        # file = "./csvs/rss.csv"
+        # type = "rss"
+        # df = is_csv_accessible(file)
+        # if df is not None:
+        #     df = is_csv_accessible(file)
+        #     urls, channels, mentions, files = get_csv_columns(df)
+        #     for i in range(len(urls)):
+        #         task = self.loop.create_task(self.automate_check(urls[i], channels[i], files[i], type, delay_offset=n*20))
+        #         n += 1
+        #         tasks.append(task)
 
         await asyncio.gather(*tasks)
 
