fb9a6a8a758fa085d414adb40f37ae38a9e5bd20
Brandon D. <84109908+BrandonDelM@users.noreply.github.com>
2026-01-08 12:45:58 -0800

    Made a function that logs functions so no update is lost. Will add Google Sheets implementation

--- main.py
+++ main.py
@@ -9,6 +9,16 @@ from checksFunctions import *
 from CSVFunctions import *
 from bluesky import *
 
+def log_updates(file, url, type, new_events):
+    if len(new_events) == 0:
+        with open("./updatelog/updatelog.txt", "a") as file:
+            file.write(f"No changes found for {type}: {url}\n")
+        return
+    file.write(f"Changes for {type}: {url}\n")
+    with open("./updatelog/updatelog.txt", "a") as file:
+        for event in new_events:
+            file.write(f"{event}\n")
+
 def check_for_changes(r, file, url, type):
     if type == "calendar":
         return calendar_changes(r, file, url)
@@ -26,26 +36,26 @@ class Client(discord.Client):
         print(f'Logged on as {self.user}!')
         self.loop.create_task(self.uc_merced_calendars_check())
     
-    # async def bluesky_check(self, channel, file, delay_offset=0):
-    #     channel = self.get_channel(channel)
-    #     if channel is None:
-    #         print(f"Error: No channel {channel} for bluesky")
-    #         return
-    #     await asyncio.sleep(delay_offset)
-    #     while not self.is_closed():
-    #         new_events = check_for_changes("", file, "", "bluesky")
+    async def bluesky_check(self, channel, file, delay_offset=0):
+        channel = self.get_channel(channel)
+        if channel is None:
+            print(f"Error: No channel {channel} for bluesky")
+            return
+        await asyncio.sleep(delay_offset)
+        while not self.is_closed():
+            new_events = check_for_changes("", file, "", "bluesky")
 
-    #         new_events_text = ""
-    #         if len(new_events) == 0:
-    #             new_events_text = f"No changes found for bluesky"
-    #         else:
-    #             new_events_text += f"Changes for bluesky feed:"
-    #             for new_event in new_events:
-    #                 if len(f"{new_events_text}{new_event}\n") > 2000:
-    #                     break
-    #                 new_events_text += f"{new_event}\n"
-    #         await channel.send(new_events_text)
-    #         await asyncio.sleep(3600)
+            new_events_text = ""
+            if len(new_events) == 0:
+                new_events_text = f"No changes found for bluesky"
+            else:
+                new_events_text += f"Changes for bluesky feed:"
+                for new_event in new_events:
+                    if len(f"{new_events_text}{new_event}\n") > 2000:
+                        break
+                    new_events_text += f"{new_event}\n"
+            await channel.send(new_events_text)
+            await asyncio.sleep(3600)
 
     async def automate_check(self, url, channel, file, type, delay_offset=0):
         channel = self.get_channel(channel)
@@ -57,17 +67,18 @@ class Client(discord.Client):
 
         while not self.is_closed():
             r = request(url)
-            if r is None and file is not "bluesky":
+            if r is None and file:
                 await channel.send(f"Failure to find calendar for {url}")
                 return
             
             new_events  = check_for_changes(r, file, url, type)
+            log_updates(file, url, type, new_events)
 
             new_events_text = ""
             if len(new_events) == 0:
                 new_events_text = f"No changes found for {type}: {url}"
             else:
-                new_events_text += f"Changes for {url}\n"
+                new_events_text += f"Changes for {type}: {url}\n"
                 for new_event in new_events:
                     if len(f"{new_events_text}{new_event}\n") > 2000:
                         break
@@ -92,8 +103,8 @@ class Client(discord.Client):
         tasks = []
         offset = 0
 
-        tasks.extend(await self.bluesky_check(1456797841803444367, "logs/bluesky/bluesky_log.txt", offset))
-        offset = len(tasks)
+        # tasks.extend(await self.bluesky_check(1456797841803444367, "logs/bluesky/bluesky_log.txt", offset))
+        # offset = len(tasks)
 
         tasks.extend(await self.get_tasks("./csvs/calendars.csv", "calendar", offset))
         offset = len(tasks)
