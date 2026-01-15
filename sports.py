84442f0629ef25ba46dda3d3f50dd6b7be890308
Brandon D. <84109908+BrandonDelM@users.noreply.github.com>
2026-01-14 18:30:58 -0800

    Created a file that looks for update on the UC Merced Bobcat team

---
+++ sports.py
@@ -0,0 +1,42 @@
+from bs4 import BeautifulSoup
+from checksFunctions import is_change, log_changes
+
+def sports_change(file):
+    data = get_sports_data()
+    news_data = data['data']
+    news = get_sports_news(news_data)
+    new_news = is_change(file, news)
+    log_changes(file, news)
+    return new_news
+
+import requests
+
+def get_sports_news(news_data):
+    news = []
+    for headline in news_data:
+        try:
+            title = headline['story_headline']
+        except:
+            title = headline
+        
+        try:
+            date = headline['story_postdate']
+        except:
+            date = headline
+        
+        try:
+            link = f"https://ucmercedbobcats.com{headline['story_path']}"
+        except:
+            link = None
+        news.append(f"{title}, {date}, {link}")
+    return news
+
+def get_sports_data():
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36"
+    }
+    url = "https://ucmercedbobcats.com/services/archives.ashx/stories?index=1&page_size=30&sport=0&season=0"
+    response = requests.get(url, headers=headers)
+    if response.status_code ==  200:
+        return response.json()
+    return None
