ca2bfa24c74c6050b5703e047d38b52fabc402b9
Brandon D. <84109908+BrandonDelM@users.noreply.github.com>
2026-01-16 08:18:17 -0800

    Testing out database functionality

---
+++ database.py
@@ -0,0 +1,70 @@
+import sqlite3
+
+def create_database():
+    conn = sqlite3.connect("database.db")
+
+def table_exists(table_name):
+    conn = sqlite3.connect("database.db")
+    c = conn.cursor()
+
+    c.execute("SELECT name FROM sqlite_master WHERE type='table' AND name=?", (table_name,))
+    item_check = c.fetchone()
+    if item_check is not None:
+        return True
+    return False
+
+
+def create_table(table_name):
+    conn = sqlite3.connect("database.db")
+    c = conn.cursor()
+
+    c.execute(f"""
+        CREATE TABLE {table_name} (
+            poster text,
+            title text,
+            start_time text,
+            end_time text,
+            building text,
+            link text
+        )
+    """)
+    conn.commit()
+    conn.close()
+
+def add_many_to_table(list, table_name):
+    conn = sqlite3.connect("database.db")
+    c = conn.cursor()
+
+    c.executemany(f"INSERT INTO {table_name} VALUES (?,?,?,?,?,?)", (list))
+
+    conn.commit()
+    conn.close()
+
+def add_to_table(table_name, poster="",title="",start_time="",end_time="",building="",link=""):
+    conn = sqlite3.connect("database.db")
+    c = conn.cursor()
+
+    c.execute(f"INSERT INTO {table_name} VALUES (?,?,?,?,?,?)", (poster,title,start_time,end_time,building,link))
+
+    conn.commit()
+    conn.close()
+
+def clear_table(table_name):
+    conn = sqlite3.connect("database.db")
+    c = conn.cursor()
+
+    c.execute(f"DELETE FROM {table_name}")
+    conn.commit()
+    conn.close()
+
+def get_all_rows_from_table(table_name,row):
+    conn = sqlite3.connect("database.db")
+    c = conn.cursor()
+
+    c.execute(f"SELECT rowid, * FROM {table_name}")
+    items = c.fetchall()
+
+    conn.commit()
+    conn.close()
+
+    return items
