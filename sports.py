c4eba87312276e1d6489cc1282125aaef95d2f06
Brandon Del Mundo <brandon.delmundo@gmail.com>
2026-01-21 12:35:37 -0800

    Forgot a library

--- sports.py
+++ sports.py
@@ -28,6 +28,7 @@ def get_sports_news(news_data):
         news.append(database_format("",title,date,"","",link))
     return news
 
+import requests
 def get_sports_data():
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36"
